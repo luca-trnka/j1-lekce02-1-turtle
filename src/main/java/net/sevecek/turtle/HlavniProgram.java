d4064ec9fc886a4dc0bb80fae914d979c61da801
luca-trnka <luie-trnkova76@seznam.cz>
2022-04-13 21:02:58 +0200

    domeček a prase

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -31,9 +31,25 @@ public class HlavniProgram {
         zofka.move(100);
         zofka.turnLeft(90);
         zofka.move(150);
+        zofka.turnLeft(30);
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.move(50);
+        zofka.penUp();
         zofka.turnLeft(60);
         zofka.move(100);
-        zofka.turnLeft(90);
+        zofka.penDown();
+        zofka.turnLeft(60);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.move(50);
+
 
 
     }
