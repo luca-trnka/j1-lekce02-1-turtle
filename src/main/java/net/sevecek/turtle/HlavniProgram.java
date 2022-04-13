b2b79741740e3a8916983628885b77531f821380
luca-trnka <luie-trnkova76@seznam.cz>
2022-04-13 20:05:53 +0200

    domeček a kus prasete

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -3,11 +3,40 @@ package net.sevecek.turtle;
 import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
+    Turtle zofka;
 
     public void main(String[] args) {
-        Turtle zofka;
-
+ /* domeček
         zofka = new Turtle();
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(67);
+        zofka.turnLeft(90);
+        zofka.move(67);
+        */
+  //prase
+        zofka = new Turtle();
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(60);
+        zofka.move(100);
+        zofka.
+
+
+
     }
 
 }
